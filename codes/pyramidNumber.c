b595241e8b8907a47b06af5497ace8b7b92babfb
Rajeev Sharma <76778621+rajeevinr@users.noreply.github.com>
2022-03-15 10:32:55 +0530

    Update pyramidNumber.c

--- codes/pyramidNumber.c
+++ codes/pyramidNumber.c
@@ -1,1 +1,40 @@
 
+#include<stdio.h>
+
+int main()
+
+{
+
+  int i,j,a;
+
+  printf("\nEnter the value:");
+
+  scanf("%d",&a);
+
+  for(i=1;i<=a;i++)
+
+  {
+
+   printf("\n");
+
+   for(j=1;j<=(a-i);j++)
+
+   {
+
+     printf(" ");
+
+   }
+
+   for(j=1;j<=i;j++)
+
+   {
+
+    printf("%d",j);
+
+   }
+
+  }
+
+  return 0;
+
+}
